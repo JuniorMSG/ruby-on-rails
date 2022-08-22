1106e33143bf35c01e891be594e5b472b9b0bb18
MSG <milk6645@gmail.com>
2022-08-23 00:21:24 +0900

    Operator end If, Loop Start

--- 02_Operator.rb
+++ 02_Operator.rb
@@ -8,9 +8,27 @@ puts a - 1
 
 a = 7
 
+# 산술 연산자
+puts "=== 산술 연산자 ==="
 puts "더하기 : a+5 = #{a+5}"
 puts "빼기  :  a-5 = #{a-5}"
 puts "곱하기 : a*5 = #{a*5}"
 puts "제곱 :  a**2 = #{a**2}"
 puts "나머지 : a%3 = #{a%3}"
 puts "나누기 : a/3 = #{a/3}"
+
+
+# 비교 연산자
+puts "=== 비교 연산자 ==="
+puts "a==b : 5==5 #{5==5}"
+puts "a!=b : 5!=4 #{5!=4}"
+puts "a<b : 5<5 #{5<5}"
+puts "a<=b : 5<=5 #{5<=5}"
+puts "a>b : 6>5 #{6>5}"
+puts "a>=b : 6>=5 #{6>=5}"
+
+# 논리 연산자
+puts "=== 논리 연산자 ==="
+puts "a && b : true&&false #{true && false}"
+puts "a || b : true||false #{true || false}"
+puts "a not b : !true #{!true}"
