fe30ccf2e2d2ff7b18e5497654c441f57f7c7a83
MSGDev <milk6645@naver.com>
2022-09-11 00:05:40 +0900

    q1000

--- algorithm/01_baekjoon_step.rb
+++ algorithm/01_baekjoon_step.rb
@@ -1,4 +1,4 @@
-
+# https://www.acmicpc.net/step/1
 # https://www.acmicpc.net/problem/2557
 # Q_2557
 # Hello World! 출력하는 문제
@@ -12,5 +12,16 @@ def Q_10718()
   puts  ('강한친구 대한육군')
 end
 
-
 Q_10718()
+
+# https://www.acmicpc.net/problem/1000
+def Q_1000()
+  str = gets.chomp
+  arr = str.split(" ")
+  numA = arr[0].to_i
+  numB = arr[1].to_i
+  puts numA + numB
+end
+
+Q_1000()
+
