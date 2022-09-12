84933612ac31b7c52f685d0008bcdf02f7aca19b
MSGDev <milk6645@naver.com>
2022-09-12 23:17:09 +0900

    algo

--- algorithm/01_baekjoon_step.rb
+++ algorithm/01_baekjoon_step.rb
@@ -12,7 +12,7 @@ def Q_10718()
   puts  ('강한친구 대한육군')
 end
 
-Q_10718()
+# Q_10718()
 
 # https://www.acmicpc.net/problem/1000
 def Q_1000()
@@ -23,5 +23,37 @@ def Q_1000()
   puts numA + numB
 end
 
-Q_1000()
+# Q_1000()
 
+# https://www.acmicpc.net/problem/1001
+def Q_1001()
+  str = gets.chomp
+  arr = str.split(" ")
+  numA = arr[0].to_i
+  numB = arr[1].to_i
+  puts numA - numB
+end
+
+# Q_1001()
+
+# https://www.acmicpc.net/problem/10998
+def Q_10998()
+  str = gets.chomp
+  arr = str.split(" ")
+  numA = arr[0].to_i
+  numB = arr[1].to_i
+  puts numA * numB
+end
+
+# Q_10998()
+
+# https://www.acmicpc.net/problem/1008
+def Q_1008()
+  str = gets.chomp
+  arr = str.split(" ")
+  numA = arr[0].to_f
+  numB = arr[1].to_f
+  puts numA / numB
+end
+
+Q_1008()
