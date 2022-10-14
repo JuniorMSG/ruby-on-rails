64ed3e4953a9ed5432c25c5f843b97742e309de9
MSG <milk6645@gmail.com>
2022-10-15 00:01:40 +0900

    Q11720 END

--- algorithm/05_baekjoon_string.rb
+++ algorithm/05_baekjoon_string.rb
@@ -5,4 +5,18 @@ def Q_11654()
   str = gets
   puts str.ord
 end
-Q_11654()
+# Q_11654()
+
+
+# https://www.acmicpc.net/problem/11720
+def Q_11720()
+  str = gets.to_i
+  list = gets.split("").map &:to_i;
+
+  sum = 0
+  for i in list
+    sum += i
+  end
+  puts sum
+end
+Q_11720()
