09878dfa8b5a589a864ffed7b726a61b3c54a604
JuniorMSG <milk6645@gmail.com>
2022-10-23 23:56:41 +0900

    user udpate

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_21_011459) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_22_143028) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -61,6 +61,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_21_011459) do
     t.datetime "updated_at", null: false
     t.string "status"
     t.string "temp"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "user_id"
+    t.string "user_name"
+    t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "articles"
