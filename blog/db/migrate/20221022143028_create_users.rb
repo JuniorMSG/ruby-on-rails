716c7414efb1e3674ccfdc60939fab9405f381e4
JuniorMSG <milk6645@gmail.com>
2022-10-23 23:54:27 +0900

    user update

--- blog/db/migrate/20221022143028_create_users.rb
+++ blog/db/migrate/20221022143028_create_users.rb
@@ -2,6 +2,9 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
 
+      t.string :user_id
+      t.string :user_name
+      t.string :phone_number
       t.timestamps
     end
   end
